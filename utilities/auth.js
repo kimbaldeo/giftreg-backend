b5bad6605d5844fa71e83338dae226da0fb29242
kbaldeoward <kbaldeoward@gmail.com>
2022-06-07 10:35:50 -0400

    auth progress, verification feature

--- utilities/auth.js
+++ utilities/auth.js
@@ -12,4 +12,28 @@ function generateToken(userInfo) {
     }) 
 }
 
+function verifyToken(username, token) {
+    return jsonWebToken.verify(token, process.env.JWT_SECRET, (error, response) => {
+        if (error) {
+            return {
+                verified: false,
+                message: 'Invalid Token'
+            }
+        }
+        
+        if (response.username !== username) {
+            return {
+                verified: false,
+                message: 'Invalid User'
+            }
+        }
+
+        return {
+            verified: true,
+            message: 'verified'
+        }
+    })
+}
+
 module.exports.generateToken = generateToken
+module.exports.verifyToken = verifyToken
